f2d1592970a888b1f5b073e2a0836453fa46703f
Anand Ramadhin <anandramadhin1@gmail.com>
2015-04-03 01:06:38 -0400

    Temperature Settings added and Share Intent added. Can now share results online.

--- app/src/main/java/com/example/anand/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/anand/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.anand.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -29,8 +31,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Anand on 29/12/2014.
@@ -64,12 +64,17 @@ public class ForecastFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-
         int id = item.getItemId();
         if(id == R.id.action_refresh)
         {
+            /*
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute();
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+            getString(R.string.pref_location_default));
+            weatherTask.execute(location);
+            */
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -79,12 +84,13 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
+    //Remove
+      /*  String[] data = {
                 "Please Refresh the Feed from the menu",
         };
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
+*/
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
         // use it to populate the ListView it's attached to.
@@ -93,7 +99,7 @@ public class ForecastFragment extends Fragment {
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
+                        new ArrayList<String>());
 
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -116,7 +122,21 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, String[]>
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                            getString(R.string.pref_location_default));
+        weatherTask.execute(location);
+    }
+
+                @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>
     {
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
@@ -133,7 +153,13 @@ public class ForecastFragment extends Fragment {
                   /**
                   * Prepare the weather high/lows for presentation.
                   */
-                        private String formatHighLows(double high, double low) {
+                        private String formatHighLows(double high, double low, String unitType) {
+                            if(unitType.equals(getString(R.string.pref_units_imperial))){
+                                high = (high * 1.8) + 32;
+                                low = (low * 1.8) + 32;
+                            } else if(!unitType.equals(getString(R.string.pref_units_metric))){
+                                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+                            }
                         // For presentation, assume the user doesn't care about tenths of a degree.
                         long roundedHigh = Math.round(high);
                         long roundedLow = Math.round(low);
@@ -182,6 +208,18 @@ public class ForecastFragment extends Fragment {
                         dayTime = new Time();
 
                         String[] resultStrs = new String[numDays];
+
+                        // Data is fetched in Celsius by default
+                        // If user prefers to see in Fahrenheit convert the values here.
+                        // We do this rather than fetching in Fahrenheit so that the user can
+                        // change this option without us having to re-fetch the data once
+                        // we start storing the values in a database
+
+                        SharedPreferences sharedPrefs =
+                                   PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        String unitType = sharedPrefs.getString(
+                                getString(R.string.pref_units_key),
+                                getString(R.string.pref_units_metric));
                         for(int i = 0; i < weatherArray.length(); i++) {
                         // For now, using the format "Day, description, hi/low"
                             String day;
@@ -211,7 +249,7 @@ public class ForecastFragment extends Fragment {
                             double high = temperatureObject.getDouble(OWM_MAX);
                             double low = temperatureObject.getDouble(OWM_MIN);
 
-                            highAndLow = formatHighLows(high, low);
+                            highAndLow = formatHighLows(high, low, unitType);
                             resultStrs[i] = day + " - " + main + " : "+ description + " - " + highAndLow;
                         }
 
@@ -222,7 +260,7 @@ public class ForecastFragment extends Fragment {
                         }
 
         @Override
-        protected String[] doInBackground(Void... params)
+        protected String[] doInBackground(String...strings)
         {
             // Pull weather data from openweathermap.org
             // These two need to be declared outside the try/catch
@@ -234,16 +272,18 @@ public class ForecastFragment extends Fragment {
             String forecastJsonStr = null;
             String format = "json";
             String units = "metric";
+            String query = "Trinidad,TT";
             int numDays = 7; //Determines the amount of days displayed on screen at one time
 
             try {
-                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Trinidad,TT";
-                //final String QUERY_PARAM = "q";
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
                 final String FORMAT_PARAM = "mode";
                 final String UNITS_PARAM = "units";
                 final String DAYS_PARAM = "cnt";
 
                 Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM,query)
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
--- app/src/main/java/com/example/anand/sunshine/MainActivity.java
+++ app/src/main/java/com/example/anand/sunshine/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.anand.sunshine;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +22,8 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +56,37 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+
+        if(id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+                // Using the URI scheme for showing a location found on a map.  This super-handy
+                // intent can is detailed in the "Common Intents" page of Android's developer site:
+                // http://developer.android.com/guide/components/intents-common.html#Maps
+                Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                        .appendQueryParameter("q", location)
+                        .build();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                    } else {
+                        Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+                    }
+            }
 
     /**
      * A placeholder fragment containing a simple view.
